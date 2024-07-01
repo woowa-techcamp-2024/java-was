858b7e1e1670ce474fa47a6b9df74c8d9213c873
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-01 20:56:51 +0900

    `HTTP Request` 파싱 및 로깅, 정적 html 응답 구현 - `clientSocket`의 `inputStream` 을 활용한 `HTTP Request` 파싱 및 로깅을 구현했습니다. - `http://localhost:8080/index.html` 접속에 대한 정적 html 응답을 구현했습니다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,7 +1,11 @@
 package codesquad;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import org.slf4j.Logger;
@@ -16,18 +20,51 @@ public class Main {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
         log.debug("Listening for connection on port 8080 ....");
 
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
+        while (true) {
+            try (Socket clientSocket = serverSocket.accept();
+                var br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                var bw = new BufferedWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream()))) {
                 log.debug("Client connected");
+                readRequest(br);
 
-                // HTTP 응답을 생성합니다.
-                OutputStream clientOutput = clientSocket.getOutputStream();
-                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-                clientOutput.write("Content-Type: text/html\r\n".getBytes());
-                clientOutput.write("\r\n".getBytes());
-                clientOutput.write("<h1>Hello</h1>\r\n".getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
-                clientOutput.flush();
+                bw.write(generateResponse());
+                bw.flush();
+            } catch (Exception e) {
+                log.error(e.getMessage());
             }
         }
+    }
+
+    private static void readRequest(BufferedReader br) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.isBlank()) {
+                break;
+            }
+            log.debug(line);
+        }
+    }
+
+    private static String generateResponse() {
+        return "HTTP/1.1 200 OK\r\n"
+            + "Content-Type: text/html\r\n"
+            + "\r\n"
+            + readIndexHTML();
+    }
+
+    private static String readIndexHTML() {
+        var sb = new StringBuilder();
+        try (var br = new BufferedReader(
+            new InputStreamReader(new FileInputStream(
+                "src/main/resources/static/index.html")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return sb.toString();
     }
 }
