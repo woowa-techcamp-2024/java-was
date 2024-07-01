11e37028c89d9f0d9381a688b1bb0828b887e0c4
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-01 16:36:41 +0900

    `Slf4j` 의존 추가 - `Slf4j` 의존을 추가했습니다. - 의존 추가에 따라 기존 존재하는 `sout`을 `Slf4j`로 변경했습니다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -4,16 +4,21 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        System.out.println("Listening for connection on port 8080 ....");
+        log.debug("Listening for connection on port 8080 ....");
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
             try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
-                System.out.println("Client connected");
+                log.debug("Client connected");
 
                 // HTTP 응답을 생성합니다.
                 OutputStream clientOutput = clientSocket.getOutputStream();
