deff8c536b46d3b1f01012f1976d124c6d0b435a
hseong3243 <hseong3243@gmail.com>
2024-07-01 22:07:31 +0900

    feat: 정적 html 파일 응답 구현
    `resource/static` 경로의 index.html 파일을 읽어 클라이언트에게 반환함.

--- src/main/java/codesquad/ClientRequest.java
+++ src/main/java/codesquad/ClientRequest.java
@@ -1,11 +1,16 @@
 package codesquad;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,13 +31,8 @@ public class ClientRequest implements Runnable {
 
             // HTTP 요청을 파싱합니다.
             InputStream clientInput = clientSocket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientInput));
-            StringBuilder sb = new StringBuilder();
-            String readLine;
-            while (!(readLine = br.readLine()).isEmpty()) {
-                sb.append(readLine).append("\n");
-            }
-            HttpRequestMessage requestMessage = HttpRequestMessage.parse(sb.toString());
+            String rawHttpRequestMessage = readHttpRequestMessage(clientInput);
+            HttpRequestMessage requestMessage = HttpRequestMessage.parse(rawHttpRequestMessage);
             log.debug("Http request message={}", requestMessage);
 
             // HTTP 응답을 생성합니다.
@@ -40,12 +40,34 @@ public class ClientRequest implements Runnable {
             clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
             clientOutput.write("Content-Type: text/html\r\n".getBytes());
             clientOutput.write("\r\n".getBytes());
-            clientOutput.write("<h1>Hello</h1>\r\n".getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
+            String rawHttpResponseMessage = getStaticFile("static/index.html");
+            clientOutput.write(rawHttpResponseMessage.getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
             clientOutput.flush();
 
             clientSocket.close();
         } catch (IOException e) {
             log.warn("입출력 예외가 발생했습니다.", e);
         }
+    }
+
+    private static String readHttpRequestMessage(InputStream clientInput) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientInput));
+        StringBuilder sb = new StringBuilder();
+        String readLine;
+        while (!(readLine = br.readLine()).isEmpty()) {
+            sb.append(readLine).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private String getStaticFile(String resourcePath) throws IOException {
+        URL resource = getClass().getClassLoader().getResource(resourcePath);
+        Path path = new File(resource.getPath()).toPath();
+        List<String> contents = Files.readAllLines(path);
+        StringBuilder sb = new StringBuilder();
+        for (String content : contents) {
+            sb.append(content).append("\n");
+        }
+        return sb.toString();
     }
 }
