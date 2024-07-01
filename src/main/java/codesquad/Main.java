5b1d530d424090f0e34739c9b0ef021aed59ea0c
minseok-oh <minseok.dev@gmail.com>
2024-07-01 19:45:41 +0900

    feat: http request 클래스 추가 후 client 입력 저장

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,27 +1,53 @@
 package codesquad;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.Objects;
 
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
+        ServerSocket serverSocket = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
 
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
+        while (true) {
+            try (Socket clientSocket = serverSocket.accept()) {
                 System.out.println("Client connected");
 
-                // HTTP 응답을 생성합니다.
-                OutputStream clientOutput = clientSocket.getOutputStream();
-                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-                clientOutput.write("Content-Type: text/html\r\n".getBytes());
-                clientOutput.write("\r\n".getBytes());
-                clientOutput.write("<h1>Hello</h1>\r\n".getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
-                clientOutput.flush();
+                InputStream clientInput = clientSocket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(clientInput);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line = bufferedReader.readLine();
+                String[] words = line.split(" ");
+                HttpRequest httpRequest = new HttpRequest(words[0], words[1], words[2]);
+                logger.debug(httpRequest.toString());
+
+                while (true) {
+                    line = bufferedReader.readLine();
+                    if (line.isEmpty()) break;
+                    httpRequest.setHeader(line);
+                }
+
+                if (Objects.equals(httpRequest.getUri(), "/index.html")) {
+                    OutputStream clientOutput = clientSocket.getOutputStream();
+                    clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    clientOutput.write("Content-Type: text/html\r\n".getBytes());
+                    clientOutput.write("\r\n".getBytes());
+
+                    int data = 0;
+                    FileInputStream fileInputStream = new FileInputStream("src/main/resources/static/index.html");
+                    byte[] buffer = new byte[fileInputStream.available()];
+                    while ((data = fileInputStream.read(buffer, 0, buffer.length)) != -1) {
+                        clientOutput.write(new String(buffer, 0, data).getBytes());
+                    }
+                    clientOutput.flush();
+                }
             }
         }
     }
