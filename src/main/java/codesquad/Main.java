9d6268ec33d8d91f30d14e976b4fbaa41b66c28d
KoKimSS <seungsu8848@ajou.ac.kr>
2024-07-01 19:43:45 +0900

    feat : 멀티 쓰레드로 Client Socket 처리하는 기능 구현

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,9 +1,9 @@
 package codesquad;
 
+import codesquad.http.Servlet.Servlet;
+import codesquad.http.log.Log;
 import codesquad.http.request.HttpRequest;
 import codesquad.http.request.HttpRequestParser;
-import codesquad.http.response.HttpResponse;
-import codesquad.http.response.HttpResponseSender;
 import codesquad.http.urlMapper.ResourceMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,60 +14,33 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static codesquad.http.response.HttpResponseSender.*;
 import static codesquad.http.urlMapper.ResourceGetter.*;
 
 
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final int PORT = 8080;
     private static final ResourceMapping resourceMapping = new ResourceMapping();
+    private static final Servlet servlet = new Servlet(resourceMapping);
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        ServerSocket serverSocket = new ServerSocket(PORT);
 
-                InputStream inputStream = clientSocket.getInputStream();
-                log.debug(inputStream.toString());
-                HttpRequest request = HttpRequestParser.parseHttpRequest(inputStream);
-                log.debug(request.toString());
-
-
-                // HTTP 응답을 생성합니다.
-                OutputStream clientOutput = clientSocket.getOutputStream();
-                String resourcePath = "";
-
-                String url = request.getUrl();
-                if(url.startsWith("/img")){
-                    resourcePath = "/static"+ url;
+        Log.log("Server started on port " + PORT);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            executorService.submit(() -> {
+                try {
+                    servlet.handleClientRequest(clientSocket);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                else {
-                    resourcePath = resourceMapping.getResourcePath(url);
-                }
-
-                System.out.println(resourcePath);
-
-                // 요청된 URL과 매핑된 리소스 파일 경로 가져오기
-                if (resourcePath != null) {
-                    if (resourcePath.endsWith(".html")) {
-                        sendHtmlResponse(clientSocket.getOutputStream(), "200 OK", "text/html", getResourceBytes(resourcePath));
-                    } else if (resourcePath.endsWith(".css")) {
-                        sendHttpResponse(clientSocket.getOutputStream(), "200 OK", "text/css", getResourceBytes(resourcePath));
-                    } else if (resourcePath.endsWith(".js")) {
-                        sendHttpResponse(clientSocket.getOutputStream(), "200 OK", "application/javascript", getResourceBytes(resourcePath));
-                    } else if (resourcePath.endsWith(".jpg") || resourcePath.endsWith(".png") || resourcePath.endsWith(".svg")) {
-                        sendImageResponse(clientSocket.getOutputStream(), "200 OK", getContentType(resourcePath), getResourceBytes(resourcePath));
-                    }
-                    System.out.println(resourcePath);
-
-                    // HTTP 응답 보내기
-                    sendHttpResponse(clientOutput, "200 OK", getContentType(resourcePath), getResourceBytes(resourcePath));
-                } else {
-                    // 요청된 리소스가 없음 (404 Not Found)
-                    sendHttpResponse(clientOutput, "404 Not Found", "text/plain", "Resource not found".getBytes(StandardCharsets.UTF_8));
-                }
-                clientOutput.flush();
-            }
+            });
         }
     }
 }
--- src/main/java/codesquad/http/log/Log.java
+++ src/main/java/codesquad/http/log/Log.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Log {
     private static final Logger logger = LoggerFactory.getLogger(Log.class);
 
-    public void log(String message) {
+    public static void log(String message) {
         logger.debug(message);
     }
 }
