cf1577a1fcb080311d6c4ccbac435435192c31cf
HiiWee <wpdlzhf159@gmail.com>
2024-07-01 19:28:17 +0900

    feat: 서버로 들어오는 HTTP Request의 모든 내용을 로그로 출력한다.
    - 일단 현재 HTTP의 모든 내용을 출력한다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,22 +1,34 @@
 package codesquad;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
         System.out.println("Listening for connection on port 8080 ....");
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
+            try (Socket clientSocket = serverSocket.accept();
+                 BufferedReader requestReader = new BufferedReader(
+                         new InputStreamReader(clientSocket.getInputStream()));
+                 OutputStream clientOutput = clientSocket.getOutputStream()) { // 클라이언트 연결을 수락합니다.
+
+                String httpRequest = printRequest(requestReader);
+                log.debug(httpRequest);
+
                 System.out.println("Client connected");
 
                 // HTTP 응답을 생성합니다.
-                OutputStream clientOutput = clientSocket.getOutputStream();
                 clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
                 clientOutput.write("Content-Type: text/html\r\n".getBytes());
                 clientOutput.write("\r\n".getBytes());
@@ -24,5 +36,16 @@ public class Main {
                 clientOutput.flush();
             }
         }
+    }
+
+    private static String printRequest(final BufferedReader requestReader) throws IOException {
+        StringBuilder httpRequestBuilder = new StringBuilder();
+        String requestLine;
+        while (!(requestLine = requestReader.readLine()).isEmpty()) {
+            httpRequestBuilder.append(requestLine)
+                    .append(System.lineSeparator());
+        }
+
+        return httpRequestBuilder.toString();
     }
 }
