9638ce8967f5cbc56c4cfe7aaa5df3d0216d6f4f
lass9436 <lass9436@gmail.com>
2024-07-02 01:58:46 +0900

    [feat] HTTP Request Headers debug 출력
    클라이언트의 HTTP 요청의 헤더 부분을 줄바꿈으로 파싱하여 로거로 출력합니다.

--- src/main/java/codesquad/server/MyServer.java
+++ src/main/java/codesquad/server/MyServer.java
@@ -3,6 +3,7 @@ package codesquad.server;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -33,19 +34,29 @@ public class MyServer {
 		while (true) {
 			try (Socket clientSocket = instance.mySocket.accept();
 				 OutputStream clientOutput = clientSocket.getOutputStream();
-				 BufferedReader br = new BufferedReader(new FileReader("src/main/resources/static/index.html"));
+				 BufferedReader clientInputReader = new BufferedReader(
+					 new InputStreamReader(clientSocket.getInputStream()));
+				 BufferedReader fileReader = new BufferedReader(new FileReader("src/main/resources/static/index.html"));
 			) {
 				// 클라이언트 연결 로그 출력
 				logger.info("Client connected");
 
-				// HTTP Status
+				// HTTP Request 로그 debug 출력
+				String requestLine;
+				StringBuilder requestBuilder = new StringBuilder();
+				while (!(requestLine = clientInputReader.readLine()).isEmpty()) {
+					requestBuilder.append(requestLine + "\r\n");
+				}
+				logger.debug(requestBuilder.toString());
+
+				// HTTP Response Status
 				clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
 				// Content-Type header
 				clientOutput.write("Content-Type: text/html\r\n".getBytes());
 				clientOutput.write("\r\n".getBytes());
 				// Response Body
 				String line = null;
-				while ((line = br.readLine()) != null) {
+				while ((line = fileReader.readLine()) != null) {
 					clientOutput.write(line.getBytes());
 					clientOutput.write("\r\n".getBytes());
 				}
