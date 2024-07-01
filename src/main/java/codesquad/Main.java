fb85f0451619c2e2978136eea3697b5c90951c98
HiiWee <wpdlzhf159@gmail.com>
2024-07-02 00:13:37 +0900

    refactor: 하나의 사용자 요청(Connection)을 처리하는 부분을 별도로 분리한다
    - Connection은 현재 index.html만 읽어옴 -> 분리 필요

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -21,33 +22,11 @@ public class Main {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
         System.out.println("Listening for connection on port 8080 ....");
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 1000L, TimeUnit.MICROSECONDS,
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.MICROSECONDS,
                 new LinkedBlockingQueue<>());
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            executor.execute(() -> {
-                try (Socket clientSocket = serverSocket.accept();
-                     BufferedReader requestReader = new BufferedReader(
-                             new InputStreamReader(clientSocket.getInputStream()));
-                     OutputStream clientOutput = clientSocket.getOutputStream();
-                     FileInputStream fileInputStream = new FileInputStream("src/main/resources/static/index.html")
-                ) { // 클라이언트 연결을 수락합니다.
-
-                    String httpRequest = printRequest(requestReader);
-                    log.debug(httpRequest);
-
-                    System.out.println("Client connected");
-
-                    // HTTP 응답을 생성합니다.
-                    clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-                    clientOutput.write("Content-Type: text/html\r\n".getBytes());
-                    clientOutput.write("\r\n".getBytes());
-                    clientOutput.write(fileInputStream.readAllBytes()); // 응답 본문으로 "Hello"를 보냅니다.
-                    clientOutput.flush();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+            executor.execute(new Connection(serverSocket.accept()));
         }
     }
 
