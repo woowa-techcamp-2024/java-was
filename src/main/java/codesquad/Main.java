3f60147068ad0d7b78f40435adc297b201843521
HiiWee <wpdlzhf159@gmail.com>
2024-07-02 01:13:33 +0900

    refactor: 인터페이스 변수로 스레드 풀 의존하도록 변경
    - 구현체를 갈아끼우기 편하도록 변경~!

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -2,6 +2,7 @@ package codesquad;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -16,7 +17,7 @@ public class Main {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
         log.debug("Listening for connection on port 8080 ....");
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.MICROSECONDS,
+        ExecutorService executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.MICROSECONDS,
                 new LinkedBlockingQueue<>());
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
