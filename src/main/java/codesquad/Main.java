1586fd7355f26ea51e0226928ff6273de0457547
HiiWee <wpdlzhf159@gmail.com>
2024-07-02 00:17:15 +0900

    refactor: println 출력을 log로 변경한다.
    - 그 외에 static 하위 폴더의 파일들의 인덴트 정렬함

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,13 +1,7 @@
 package codesquad;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -20,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        System.out.println("Listening for connection on port 8080 ....");
+        log.debug("Listening for connection on port 8080 ....");
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.MICROSECONDS,
                 new LinkedBlockingQueue<>());
@@ -28,16 +22,5 @@ public class Main {
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
             executor.execute(new Connection(serverSocket.accept()));
         }
-    }
-
-    private static String printRequest(final BufferedReader requestReader) throws IOException {
-        StringBuilder httpRequestBuilder = new StringBuilder();
-        String requestLine;
-        while (!(requestLine = requestReader.readLine()).isEmpty()) {
-            httpRequestBuilder.append(requestLine)
-                    .append(System.lineSeparator());
-        }
-
-        return httpRequestBuilder.toString();
     }
 }
