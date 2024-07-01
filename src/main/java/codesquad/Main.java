3dfe673590c92bfe08ed1f51820f1ac0e29ba9fa
hellomatia <shuaigejp80@gmail.com>
2024-07-01 18:42:11 +0900

    8080포트로 사용자의 요청을 받으면, logger로 HTTP method, URL, Protocol을 표시하는 기능 추가

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -2,8 +2,8 @@ package codesquad;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import java.io.OutputStream;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,9 +15,17 @@ public class Main {
         logger.debug("Listening for connection on port 8080 ....");
 
         while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
+            try (Socket clientSocket = serverSocket.accept();
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) { // 클라이언트 연결을 수락합니다.
+                String request = in.readLine();
                 logger.debug("Client connected");
-
+                if (!request.isEmpty()) {
+                    String[] requestParts = request.split(" ");
+                    logger.debug("Client request HTTP method: {}", requestParts[0]);
+                    logger.debug("Client request URL: {}", requestParts[1]);
+                    logger.debug("Client request HTTP Protocol: {}", requestParts[2]);
+                }
                 // HTTP 응답을 생성합니다.
                 OutputStream clientOutput = clientSocket.getOutputStream();
                 clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
