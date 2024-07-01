17c044ac04d746d5c8a770b9fedc74288a4d8402
lass9436 <lass9436@gmail.com>
2024-07-02 00:25:53 +0900

    [feat] MyServer 와 MySocket 작성
    main 의 로직을 MyServer 와 MySocket 으로 분리했습니다. MySocket 은 ServerSocket 을 상속받습니다.

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,28 +1,14 @@
 package codesquad;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
+import codesquad.server.MyServer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        System.out.println("Listening for connection on port 8080 ....");
 
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
-                System.out.println("Client connected");
+	public static void main(String[] args) throws IOException {
 
-                // HTTP 응답을 생성합니다.
-                OutputStream clientOutput = clientSocket.getOutputStream();
-                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-                clientOutput.write("Content-Type: text/html\r\n".getBytes());
-                clientOutput.write("\r\n".getBytes());
-                clientOutput.write("<h1>Hello</h1>\r\n".getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
-                clientOutput.flush();
-            }
-        }
-    }
+		MyServer server = new MyServer();
+		server.start();
+	}
 }
---
+++ src/main/java/codesquad/server/MyServer.java
@@ -0,0 +1,44 @@
+package codesquad.server;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import codesquad.Main;
+import codesquad.socket.MySocket;
+
+public class MyServer {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	private static final int PORT = 8080;
+
+	private MySocket mySocket;
+
+	public MyServer() {
+		try {
+			mySocket = new MySocket(PORT);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+	public void start() throws IOException {
+		while (true) {
+			try (Socket clientSocket = mySocket.accept()) {
+				logger.info("Client connected");
+
+				// HTTP 응답을 생성합니다.
+				OutputStream clientOutput = clientSocket.getOutputStream();
+				clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+				clientOutput.write("Content-Type: text/html\r\n".getBytes());
+				clientOutput.write("\r\n".getBytes());
+				clientOutput.write("<h1>Hello</h1>\r\n".getBytes()); // 응답 본문으로 "Hello"를 보냅니다.
+				clientOutput.flush();
+			}
+		}
+	}
+}
