a65a8bee52ed327112ade9696967fef5e2eeba96
hseong3243 <hseong3243@gmail.com>
2024-07-01 22:53:03 +0900

    fix: 스레드 풀을 상수로 변경
    기존 스레드풀 지역 변수로 선언한 것에서 상수로 변경함.

--- src/main/java/codesquad/Server.java
+++ src/main/java/codesquad/Server.java
@@ -3,18 +3,20 @@ package codesquad;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public final class Server {
 
+    private static final ExecutorService EXECUTOR_SERVICE = new ThreadPoolExecutor(10, 200, 60, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(50));
+
     public static void run(ServerSocket serverSocket) throws IOException {
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(10, 200, 60, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(50));
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            threadPool.execute(new ClientRequest(clientSocket));
+            EXECUTOR_SERVICE.execute(new ClientRequest(clientSocket));
         }
     }
 
