a973627bf74381762d70f0b4f3b7f89dd4092233
lass9436 <lass9436@gmail.com>
2024-07-02 02:55:54 +0900

    [feat] 멀티스레드 처리로 변경
    Runnable 인터페이스를 구현한 MyRunnable 클래스를 작성하였습니다.
    run 메소드를 오버라이드 하여 원래 MyServer 에 있던 로직을 이양했습니다.
    MyServer 는 while 문을 돌면서 threadPool 에 할당하는 역할만 담당하게 됩니다.

--- src/main/java/codesquad/server/MyServer.java
+++ src/main/java/codesquad/server/MyServer.java
@@ -1,25 +1,23 @@
 package codesquad.server;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import codesquad.Main;
+import codesquad.runnable.MyRunnable;
 import codesquad.socket.MySocket;
 
 public class MyServer {
 
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	private static final MyServer instance = new MyServer();
+	private static final Logger logger = LoggerFactory.getLogger(MyServer.class);
+	private static final int THREAD_POOL_SIZE = 10;
+	private static final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
 	private final int PORT = 8080;
-
 	private MySocket mySocket;
 
 	private MyServer() {
@@ -32,38 +30,7 @@ public class MyServer {
 
 	public static void start() throws IOException {
 		while (true) {
-			try (Socket clientSocket = instance.mySocket.accept();
-				 OutputStream clientOutput = clientSocket.getOutputStream();
-				 BufferedReader clientInputReader = new BufferedReader(
-					 new InputStreamReader(clientSocket.getInputStream()));
-				 BufferedReader fileReader = new BufferedReader(new FileReader("src/main/resources/static/index.html"));
-			) {
-				// 클라이언트 연결 로그 출력
-				logger.info("Client connected");
-
-				// HTTP Request 로그 debug 출력
-				String requestLine;
-				StringBuilder requestBuilder = new StringBuilder();
-				while (!(requestLine = clientInputReader.readLine()).isEmpty()) {
-					requestBuilder.append(requestLine + "\r\n");
-				}
-				logger.debug(requestBuilder.toString());
-
-				// HTTP Response Status
-				clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-				// Content-Type header
-				clientOutput.write("Content-Type: text/html\r\n".getBytes());
-				clientOutput.write("\r\n".getBytes());
-				// Response Body
-				String line = null;
-				while ((line = fileReader.readLine()) != null) {
-					clientOutput.write(line.getBytes());
-					clientOutput.write("\r\n".getBytes());
-				}
-
-				// write flush
-				clientOutput.flush();
-			}
+			threadPool.submit(new MyRunnable(instance.mySocket.accept()));
 		}
 	}
 }
